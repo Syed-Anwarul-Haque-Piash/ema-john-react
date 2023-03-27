8a8fb7203918ad64d8203ef5accdf3c600ff2eeb
Syed-Anwarul-Haque-Piash <syed88783@gmail.com>
2023-03-27 23:40:02 +0600

    half done

--- ema-john/src/components/Cart/Cart.jsx
+++ ema-john/src/components/Cart/Cart.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = (props) => {
     const cart=props.cart;
+
+    let total=0;
+    let totalShipping=0;
+    for(const product of cart){
+        total=total+product.price;
+        totalShipping=totalShipping+product.shipping;
+    }
+    const tax=total*7/100;
+    const grandTotal=total+totalShipping+tax;
     return (
-        <div>
+        <div className='cart'>
             <h1>Order Summary</h1>
             <p>Selected Items :{cart.length}</p>
+            <p>Total Price: ${total}</p>
+            <p>Total Shipping Charge: ${totalShipping}</p>
+            <p>Tax :${tax}</p>
+            <p>Grandtotal: ${grandTotal}</p>
         </div>
     );
 };

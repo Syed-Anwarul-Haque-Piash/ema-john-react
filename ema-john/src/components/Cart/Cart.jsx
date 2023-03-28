3c4810ad13683b64e8561a8fc2709ffa511ed254
Syed-Anwarul-Haque-Piash <syed88783@gmail.com>
2023-03-28 21:22:43 +0600

    show all data by localstorage

--- ema-john/src/components/Cart/Cart.jsx
+++ ema-john/src/components/Cart/Cart.jsx
@@ -6,16 +6,20 @@ const Cart = (props) => {
 
     let total=0;
     let totalShipping=0;
+    let quantity=0;
+    
     for(const product of cart){
-        total=total+product.price;
+        product.quantity=product.quantity || 1;
+        total=total+product.price*product.quantity;
         totalShipping=totalShipping+product.shipping;
+        quantity=quantity+product.quantity;
     }
     const tax=total*7/100;
     const grandTotal=total+totalShipping+tax;
     return (
         <div className='cart'>
             <h1>Order Summary</h1>
-            <p>Selected Items :{cart.length}</p>
+            <p>Selected Items :{quantity}</p>
             <p>Total Price: ${total}</p>
             <p>Total Shipping Charge: ${totalShipping}</p>
             <p>Tax :${tax}</p>
